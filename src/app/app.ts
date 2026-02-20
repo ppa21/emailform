6083d779bafe71f5e0927a16c002ca7b144dbf77
ppa21 <pruthviraj1998@hotmail.com>
2026-02-20 14:45:07 -0800

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('emailform');
+  email: string = '';
 }
